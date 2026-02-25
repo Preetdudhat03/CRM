b25edaad61edc169857dd79252e303d0176bf21f
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-02-25 19:48:11 +0530

    25/2/2026

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.crm.app.field_crm"
+    namespace = "com.example.field_crm"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.crm.app.field_crm"
+        applicationId = "com.example.field_crm"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
