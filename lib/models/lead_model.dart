65e00530dccc72fa4bf09552c237bd88de55d741
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-02-27 17:40:16 +0530

    27/2/2026

--- lib/models/lead_model.dart
+++ lib/models/lead_model.dart
@@ -126,7 +126,6 @@ class LeadModel {
     final lastName = nameParts.length > 1 ? nameParts.sublist(1).join(' ') : '';
     
     return {
-      'name': name,
       'first_name': firstName,
       'last_name': lastName,
       'email': email,
